95d3a0428c03fb19eabb9e82f14f9d1915b190c4
André Rodrigues <androd97@gmail.com>
2022-06-16 19:27:56 +0100

    Add Material and Renderable structs

---
+++ src/Graphics/Material.h
@@ -0,0 +1,17 @@
+//
+// Created by Andre Rodrigues on 16/06/2022.
+//
+
+#pragma once
+
+#include "Core/VulkanBase.h"
+
+namespace Wraith {
+
+    struct Material {
+        VkPipeline pipeline;
+        VkPipelineLayout pipelineLayout;
+    };
+}
+
+
