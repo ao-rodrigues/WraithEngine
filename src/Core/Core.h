dc141766d9a399fa6168bfec025854fd3e2add6f
André Rodrigues <androd97@gmail.com>
2022-06-16 18:44:55 +0100

    Modify WR_ASSERT to abort the program if it fails

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -9,8 +9,11 @@
 
 #ifdef WR_CONFIG_DEBUG
 
-// TODO break at the line when the assert happens
-#define WR_ASSERT(x, msg) if (!(x)) { WR_LOG_FATAL("[{}:{}] Assert {} failed: {}", __FILE__, __LINE__, #x, msg) }
+#define WR_ASSERT(x, msg)                                                           \
+    if (!(x)) {                                                                     \
+        WR_LOG_FATAL("[{}:{}] Assert {} failed: {}", __FILE__, __LINE__, #x, msg);  \
+        abort();                                                                    \
+    }                                                                               \
 
 #else
 
