4a8e3487090d15ec8850caa2c09ed8c89a107913
André Rodrigues <androd97@gmail.com>
2022-06-16 18:47:13 +0100

    Fix weird bug where backfaces were still showing

--- src/Utils/VkFactory.cpp
+++ src/Utils/VkFactory.cpp
@@ -67,7 +67,7 @@ namespace Wraith::VkFactory {
         rasterizer.rasterizerDiscardEnable = VK_FALSE;
         rasterizer.polygonMode = polygonMode;
         rasterizer.lineWidth = 1.0f;
-        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterizer.cullMode = VK_CULL_MODE_NONE;
         rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
         rasterizer.depthBiasEnable = VK_FALSE;
         rasterizer.depthBiasConstantFactor = 0.0f; // Optional
