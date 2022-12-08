cdd9265298e0bc66b93581f7135f7eabb1184620
André Rodrigues <androd97@gmail.com>
2022-12-08 14:30:29 +0000

    Transform SwapChain into a Wraith object and remove some of its responsibilities
    The SwapChain class had a lot of code which should be moved into the Renderer, to create a better distinction of responsibilities.

--- src/Graphics/SwapChain.cpp
+++ src/Graphics/SwapChain.cpp
@@ -7,83 +7,40 @@
 #include "Utils/VkFactory.h"
 
 namespace Wraith {
-    SwapChain::SwapChain(Device& device, Window& window)
-            : _device(device), _window(window) {
+
+    void SwapChain::Create(const Device& device, const Window& window) {
+        _device = device;
+        _window = window;
+
         CreateSwapChain();
         CreateImageViews();
         CreateDepthImage();
-        CreateRenderPass();
-        CreateFramebuffers();
-        CreateSyncObjects();
     }
 
-    SwapChain::~SwapChain() {
-        for (const auto framebuffer: _swapChainFramebuffers) {
-            vkDestroyFramebuffer(_device.GetVkDevice(), framebuffer, nullptr);
-        }
+    void SwapChain::Destroy() {
+        vkDestroySwapchainKHR(_device->GetVkDevice(), _swapChain, nullptr);
 
         for (const auto imageView: _swapChainImageViews) {
-            vkDestroyImageView(_device.GetVkDevice(), imageView, nullptr);
+            vkDestroyImageView(_device->GetVkDevice(), imageView, nullptr);
         }
 
-        vkDestroyImageView(_device.GetVkDevice(), _depthImageView, nullptr);
-        vmaDestroyImage(_device.GetAllocator(), _depthImage, _depthImageAllocation);
-
-        vkDestroyRenderPass(_device.GetVkDevice(), _renderPass, nullptr);
-        vkDestroySwapchainKHR(_device.GetVkDevice(), _swapChain, nullptr);
-
-        for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
-            vkDestroySemaphore(_device.GetVkDevice(), _imageAvailableSemaphores[i], nullptr);
-            vkDestroySemaphore(_device.GetVkDevice(), _renderFinishedSemaphores[i], nullptr);
-            vkDestroyFence(_device.GetVkDevice(), _inFlightFences[i], nullptr);
-        }
+        vkDestroyImageView(_device->GetVkDevice(), _depthImageView, nullptr);
+        vmaDestroyImage(_device->GetAllocator(), _depthImage, _depthImageAllocation);
     }
 
-    VkResult SwapChain::AcquireNextImage(uint32_t* imageIndex) const {
-        vkWaitForFences(_device.GetVkDevice(), 1, &_inFlightFences[_currentFrame], VK_TRUE, UINT64_MAX);
-        return vkAcquireNextImageKHR(_device.GetVkDevice(), _swapChain, UINT64_MAX,
-                                     _imageAvailableSemaphores[_currentFrame], VK_NULL_HANDLE, imageIndex);
+    bool SwapChain::IsUndefined() const {
+        return _swapChain == VK_NULL_HANDLE;
     }
 
-    VkResult SwapChain::SubmitCommandBuffers(const VkCommandBuffer* buffers, uint32_t* imageIndex) {
-        // Check if a previous frame is using this image (i.e. there is its' fence to wait on)
-        if (_imagesInFlight[*imageIndex] != VK_NULL_HANDLE) {
-            vkWaitForFences(_device.GetVkDevice(), 1, &_imagesInFlight[*imageIndex], VK_TRUE, UINT64_MAX);
-        }
-        // Mark the image as now being in use by this frame
-        _imagesInFlight[*imageIndex] = _inFlightFences[_currentFrame];
+    VkResult SwapChain::AcquireNextImage(uint32_t* imageIndex, uint64_t timeout, VkSemaphore signalSemaphore) const {
+        return vkAcquireNextImageKHR(_device->GetVkDevice(), _swapChain, timeout, signalSemaphore, VK_NULL_HANDLE, imageIndex);
+    }
 
-        VkSubmitInfo submitInfo{};
-        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-
-        const VkSemaphore waitSemaphores[] = {
-                _imageAvailableSemaphores[_currentFrame]
-        };
-        const VkPipelineStageFlags waitStages[] = {
-                VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
-        };
-        submitInfo.waitSemaphoreCount = 1;
-        submitInfo.pWaitSemaphores = waitSemaphores;
-        submitInfo.pWaitDstStageMask = waitStages;
-        submitInfo.commandBufferCount = 1;
-        submitInfo.pCommandBuffers = buffers;
-
-        const VkSemaphore signalSemaphores[] = {
-                _renderFinishedSemaphores[_currentFrame]
-        };
-        submitInfo.signalSemaphoreCount = 1;
-        submitInfo.pSignalSemaphores = signalSemaphores;
-
-        vkResetFences(_device.GetVkDevice(), 1, &_inFlightFences[_currentFrame]);
-
-        if (vkQueueSubmit(_device.GetGraphicsQueue(), 1, &submitInfo, _inFlightFences[_currentFrame]) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to submit draw command buffer!");
-        }
-
+    VkResult SwapChain::Present(VkSemaphore waitSemaphore, const uint32_t* imageIndex) {
         VkPresentInfoKHR presentInfo{};
         presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
         presentInfo.waitSemaphoreCount = 1;
-        presentInfo.pWaitSemaphores = signalSemaphores;
+        presentInfo.pWaitSemaphores = &waitSemaphore;
 
         const VkSwapchainKHR swapChains[] = {
                 _swapChain
@@ -93,14 +50,11 @@ namespace Wraith {
         presentInfo.pImageIndices = imageIndex;
         presentInfo.pResults = nullptr; // Optional
 
-        const VkResult result = vkQueuePresentKHR(_device.GetPresentQueue(), &presentInfo);
-        _currentFrame = (_currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
-
-        return result;
+        return vkQueuePresentKHR(_device->GetPresentQueue(), &presentInfo);
     }
 
     void SwapChain::CreateSwapChain() {
-        const Device::SwapChainSupportDetails swapChainSupport = _device.GetSwapChainSupport();
+        const Device::SwapChainSupportDetails swapChainSupport = _device->GetSwapChainSupport();
 
         const VkSurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(swapChainSupport.formats);
         const VkPresentModeKHR presentMode = ChooseSwapPresentMode(swapChainSupport.presentModes);
@@ -115,7 +69,7 @@ namespace Wraith {
 
         VkSwapchainCreateInfoKHR createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
-        createInfo.surface = _device.GetSurface();
+        createInfo.surface = _device->GetSurface();
         createInfo.minImageCount = imageCount;
         createInfo.imageFormat = surfaceFormat.format;
         createInfo.imageColorSpace = surfaceFormat.colorSpace;
@@ -123,7 +77,7 @@ namespace Wraith {
         createInfo.imageArrayLayers = 1;
         createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 
-        const Device::QueueFamilyIndices indices = _device.GetQueueFamilyIndices();
+        const Device::QueueFamilyIndices indices = _device->GetQueueFamilyIndices();
         const uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
         if (indices.graphicsFamily != indices.presentFamily) {
             createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
@@ -141,14 +95,14 @@ namespace Wraith {
         createInfo.clipped = VK_TRUE;
         createInfo.oldSwapchain = VK_NULL_HANDLE;
 
-        if (vkCreateSwapchainKHR(_device.GetVkDevice(), &createInfo, nullptr, &_swapChain) != VK_SUCCESS) {
+        if (vkCreateSwapchainKHR(_device->GetVkDevice(), &createInfo, nullptr, &_swapChain) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create swap chain!");
         }
         WR_LOG_DEBUG("Created swap chain.");
 
-        vkGetSwapchainImagesKHR(_device.GetVkDevice(), _swapChain, &imageCount, nullptr);
+        vkGetSwapchainImagesKHR(_device->GetVkDevice(), _swapChain, &imageCount, nullptr);
         _swapChainImages.resize(imageCount);
-        vkGetSwapchainImagesKHR(_device.GetVkDevice(), _swapChain, &imageCount, _swapChainImages.data());
+        vkGetSwapchainImagesKHR(_device->GetVkDevice(), _swapChain, &imageCount, _swapChainImages.data());
 
         _swapChainImageFormat = surfaceFormat.format;
         _swapChainExtent = extent;
@@ -159,7 +113,7 @@ namespace Wraith {
         for (size_t i = 0; i < _swapChainImages.size(); i++) {
             VkImageViewCreateInfo createInfo = VkFactory::ImageViewCreateInfo(_swapChainImageFormat, _swapChainImages[i], VK_IMAGE_ASPECT_COLOR_BIT);
 
-            if (vkCreateImageView(_device.GetVkDevice(), &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) {
+            if (vkCreateImageView(_device->GetVkDevice(), &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) {
                 throw std::runtime_error("Failed to create image views!");
             }
         }
@@ -179,130 +133,11 @@ namespace Wraith {
         allocationCreateInfo.usage = VMA_MEMORY_USAGE_GPU_ONLY;
         allocationCreateInfo.requiredFlags = VkMemoryPropertyFlags(VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
-        vmaCreateImage(_device.GetAllocator(), &imageCreateInfo, &allocationCreateInfo, &_depthImage, &_depthImageAllocation, nullptr);
+        vmaCreateImage(_device->GetAllocator(), &imageCreateInfo, &allocationCreateInfo, &_depthImage, &_depthImageAllocation, nullptr);
 
         VkImageViewCreateInfo imageViewCreateInfo = VkFactory::ImageViewCreateInfo(_depthImageFormat, _depthImage, VK_IMAGE_ASPECT_DEPTH_BIT);
-        if (vkCreateImageView(_device.GetVkDevice(), &imageViewCreateInfo, nullptr, &_depthImageView) != VK_SUCCESS) {
+        if (vkCreateImageView(_device->GetVkDevice(), &imageViewCreateInfo, nullptr, &_depthImageView) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create depth image view!");
-        }
-    }
-
-    void SwapChain::CreateRenderPass() {
-        VkAttachmentDescription colorAttachment{};
-        colorAttachment.format = _swapChainImageFormat;
-        colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-        colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-        colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-
-        VkAttachmentReference colorAttachmentRef{};
-        colorAttachmentRef.attachment = 0;
-        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-        VkAttachmentDescription depthAttachment{};
-        depthAttachment.format = _depthImageFormat;
-        depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-        depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-        depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        depthAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-        VkAttachmentReference depthAttachmentRef{};
-        depthAttachmentRef.attachment = 1;
-        depthAttachmentRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-        VkSubpassDescription subpass{};
-        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-        subpass.colorAttachmentCount = 1;
-        subpass.pColorAttachments = &colorAttachmentRef;
-        subpass.pDepthStencilAttachment = &depthAttachmentRef;
-
-        VkAttachmentDescription attachments[2] = {colorAttachment, depthAttachment};
-
-        VkRenderPassCreateInfo renderPassInfo{};
-        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-        renderPassInfo.attachmentCount = 2;
-        renderPassInfo.pAttachments = &attachments[0];
-        renderPassInfo.subpassCount = 1;
-        renderPassInfo.pSubpasses = &subpass;
-
-        VkSubpassDependency dependency{};
-        dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-        dependency.dstSubpass = 0;
-        dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-        dependency.srcAccessMask = 0;
-        dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-        dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
-
-        VkSubpassDependency depthDependency{};
-        depthDependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-        depthDependency.dstSubpass = 0;
-        depthDependency.srcStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
-        depthDependency.srcAccessMask = 0;
-        depthDependency.dstStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
-        depthDependency.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-
-        VkSubpassDependency dependencies[2] = {dependency, depthDependency};
-
-        renderPassInfo.dependencyCount = 2;
-        renderPassInfo.pDependencies = &dependencies[0];
-
-        if (vkCreateRenderPass(_device.GetVkDevice(), &renderPassInfo, nullptr, &_renderPass) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to create render pass!");
-        }
-        WR_LOG_DEBUG("Created render pass.")
-    }
-
-    void SwapChain::CreateFramebuffers() {
-        _swapChainFramebuffers.resize(_swapChainImageViews.size());
-        for (size_t i = 0; i < _swapChainImageViews.size(); i++) {
-            VkImageView attachments[2] = {
-                    _swapChainImageViews[i],
-                    _depthImageView
-            };
-
-            VkFramebufferCreateInfo framebufferInfo{};
-            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-            framebufferInfo.renderPass = _renderPass;
-            framebufferInfo.attachmentCount = 2;
-            framebufferInfo.pAttachments = attachments;
-            framebufferInfo.width = _swapChainExtent.width;
-            framebufferInfo.height = _swapChainExtent.height;
-            framebufferInfo.layers = 1;
-
-            if (vkCreateFramebuffer(_device.GetVkDevice(), &framebufferInfo, nullptr, &_swapChainFramebuffers[i]) != VK_SUCCESS) {
-                throw std::runtime_error("Failed to create framebuffer!");
-            }
-        }
-    }
-
-    void SwapChain::CreateSyncObjects() {
-        _imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
-        _renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
-        _inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
-        _imagesInFlight.resize(_swapChainImages.size(), VK_NULL_HANDLE);
-
-        VkSemaphoreCreateInfo semaphoreInfo{};
-        semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-
-        VkFenceCreateInfo fenceInfo{};
-        fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-        fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
-
-        for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
-            if (vkCreateSemaphore(_device.GetVkDevice(), &semaphoreInfo, nullptr, &_imageAvailableSemaphores[i]) !=
-                VK_SUCCESS ||
-                    vkCreateSemaphore(_device.GetVkDevice(), &semaphoreInfo, nullptr, &_renderFinishedSemaphores[i]) !=
-                    VK_SUCCESS ||
-                vkCreateFence(_device.GetVkDevice(), &fenceInfo, nullptr, &_inFlightFences[i]) != VK_SUCCESS) {
-                throw std::runtime_error("Failed to create synchronization objects for a frame!");
-            }
-            WR_LOG_DEBUG("Created synchronization objects for frame.")
         }
     }
 
@@ -332,7 +167,7 @@ namespace Wraith {
             return capabilities.currentExtent;
         } else {
             int width, height = 0;
-            _window.GetFramebufferSize(&width, &height);
+            _window->GetFramebufferSize(&width, &height);
 
             VkExtent2D actualExtent = {
                     static_cast<uint32_t>(width),
@@ -345,4 +180,5 @@ namespace Wraith {
             return actualExtent;
         }
     }
+
 }
--- src/Graphics/SwapChain.h
+++ src/Graphics/SwapChain.h
@@ -1,42 +1,40 @@
 ﻿#pragma once
 
 #include "Core/VulkanBase.h"
+#include "Graphics/Device.h"
+#include "Wraith/Object.h"
 
 namespace Wraith {
-    class Window;
 
-    class Device;
-
-    class SwapChain {
+    class SwapChain : public Object<SwapChain> {
     public:
-        explicit SwapChain(Device& device, Window& window);
-        ~SwapChain();
+        SwapChain() = default;
 
-        SwapChain(const SwapChain&) = delete;
-        SwapChain& operator=(const SwapChain&) = delete;
+        void Create(const Device& device, const Window& window);
+        void Destroy();
 
-        VkResult AcquireNextImage(uint32_t* imageIndex) const;
-        VkResult SubmitCommandBuffers(const VkCommandBuffer* buffers, uint32_t* imageIndex);
+        bool IsUndefined() const override;
 
         VkExtent2D GetExtent() const { return _swapChainExtent; }
-        VkRenderPass GetRenderPass() const { return _renderPass; }
-        VkFramebuffer GetFramebuffer(uint32_t imageIndex) const { return _swapChainFramebuffers[imageIndex]; }
+        VkFormat GetImageFormat() const { return _swapChainImageFormat; }
+        VkFormat GetDepthImageFormat() const { return _depthImageFormat; }
+        const std::vector<VkImageView>& GetImageViews() const { return _swapChainImageViews; }
+        VkImageView GetDepthImageView() const { return _depthImageView; }
 
-        static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
+        VkResult AcquireNextImage(uint32_t* imageIndex, uint64_t timeout, VkSemaphore signalSemaphore) const;
+        VkResult Present(VkSemaphore waitSemaphore, const uint32_t* imageIndex);
+
     private:
         void CreateSwapChain();
         void CreateImageViews();
         void CreateDepthImage();
-        void CreateRenderPass();
-        void CreateFramebuffers();
-        void CreateSyncObjects();
 
         VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
         VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
         VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
-        Device& _device;
-        Window& _window;
+        Device::Ref _device;
+        Window::Ref _window;
 
         VkSwapchainKHR _swapChain = VK_NULL_HANDLE;
 
@@ -48,17 +46,8 @@ namespace Wraith {
 
         VkExtent2D _swapChainExtent{};
         std::vector<VkImageView> _swapChainImageViews;
+
         VkImageView _depthImageView{};
-        std::vector<VkFramebuffer> _swapChainFramebuffers;
-
-        VkRenderPass _renderPass = VK_NULL_HANDLE;
-
-        VmaAllocation _depthImageAllocation;
-
-        std::vector<VkSemaphore> _imageAvailableSemaphores;
-        std::vector<VkSemaphore> _renderFinishedSemaphores;
-        std::vector<VkFence> _inFlightFences;
-        std::vector<VkFence> _imagesInFlight;
-        size_t _currentFrame = 0;
+        VmaAllocation _depthImageAllocation = VK_NULL_HANDLE;
     };
 }
