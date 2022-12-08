c6cc44f29832a9112fe035e68af5213fc6668823
André Rodrigues <androd97@gmail.com>
2022-12-08 22:58:04 +0000

    Move vulkan checking macros into the Vulkan header

--- src/Core/Core.h
+++ src/Core/Core.h
@@ -6,23 +6,3 @@
 
 #include "Wraith/Logger.h"
 #include "Utils.h"
-
-
-#define WR_VK_CHECK(x)                                                          \
-{                                                                               \
-    VkResult err = x;                                                           \
-    if (err != VK_SUCCESS) {                                                    \
-        WR_LOG_ERROR("Vulkan error: {}", err);                                  \
-        abort();                                                                \
-    }                                                                           \
-}                                                                               \
-
-
-#define WR_VK_CHECK_MSG(x, msg)                                                 \
-{                                                                               \
-    VkResult err = x;                                                           \
-    if (err != VK_SUCCESS) {                                                    \
-        WR_LOG_ERROR("Vulkan error {}: {}", err, msg);                          \
-        abort();                                                                \
-    }                                                                           \
-}                                                                               \
