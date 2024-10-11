78f31666b7d60624dc116cf180be11b6ca7cce44
André Rodrigues <androd97@gmail.com>
2024-10-11 21:24:04 +0100

    Add Essential header

---
+++ Engine/src/Wraith/Essential.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * Header file meant to be included or reachable in every file.
+ * It's meant to be lightweight and just include the bare essentials.
+ */
+
+#include "Core/LiteralTypes.h"
+#include "Core/ConfigMacros.h"
+#include "Core/PlatformMacros.h"
